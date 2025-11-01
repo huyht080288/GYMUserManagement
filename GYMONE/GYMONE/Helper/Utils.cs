c2557af829db63396bf7d32d01bd3bd8e76747e4
huyht-bitbucket-cloud <huyht080288@gmail.com>
2025-11-01 17:20:33 +0700

    Add UnitTest code

---
+++ GYMONE/GYMONE/Helper/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GYMONE.Helper
+{
+    public class Utils
+    {
+        public string GetErrorCode(int nCode)
+        {
+            if (nCode == 0)
+                return "Success";
+            else if (nCode == -1)
+                return "Unknown Error";
+            else if (nCode == -2)
+                return "Connection Error";
+            else if (nCode == -3)
+                return "Timeout Error";
+            else
+                return "Undefined Error Code";
+        }
+        public bool IsAdminUser(string userName)
+        {
+            // Placeholder logic for checking if a user is an admin
+            var adminUsers = new List<string> { "admin", "superuser", "manager" };
+            return adminUsers.Contains(userName.ToLower());
+        }
+    }
+}
---
+++ GYMONE/GYMONETests/Helper/UtilsTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GYMONE.Helper;
+
+namespace GYMONE.Helper.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        private Utils _utils;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _utils = new Utils();
+        }
+
+        [TestMethod]
+        public void GetErrorCode_ReturnsSuccess_WhenCodeIsZero()
+        {
+            var result = _utils.GetErrorCode(0);
+            Assert.AreEqual("Success", result);
+        }
+
+        [TestMethod]
+        public void GetErrorCode_ReturnsUnknownError_WhenCodeIsMinusOne()
+        {
+            var result = _utils.GetErrorCode(-1);
+            Assert.AreEqual("Unknown Error", result);
+        }
+
+        [TestMethod]
+        public void GetErrorCode_ReturnsConnectionError_WhenCodeIsMinusTwo()
+        {
+            var result = _utils.GetErrorCode(-2);
+            Assert.AreEqual("Connection Error", result);
+        }
+
+        [TestMethod]
+        public void GetErrorCode_ReturnsTimeoutError_WhenCodeIsMinusThree()
+        {
+            var result = _utils.GetErrorCode(-3);
+            Assert.AreEqual("Timeout Error", result);
+        }
+
+        [TestMethod]
+        public void GetErrorCode_ReturnsUndefinedErrorCode_ForOtherValues()
+        {
+            var result = _utils.GetErrorCode(123);
+            Assert.AreEqual("Undefined Error Code", result);
+        }
+
+        // Tests for IsAdminUser
+        [TestMethod]
+        public void IsAdminUser_ReturnsTrue_ForAdminExact()
+        {
+            var result = _utils.IsAdminUser("admin");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsAdminUser_ReturnsTrue_ForAdminDifferentCase()
+        {
+            var result = _utils.IsAdminUser("Admin");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsAdminUser_ReturnsTrue_ForOtherAdminNames()
+        {
+            Assert.IsTrue(_utils.IsAdminUser("superuser"));
+            Assert.IsTrue(_utils.IsAdminUser("Manager")); // case-insensitive
+        }
+
+        [TestMethod]
+        public void IsAdminUser_ReturnsFalse_ForNonAdmin()
+        {
+            var result = _utils.IsAdminUser("regularuser");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsAdminUser_ReturnsFalse_ForEmptyString()
+        {
+            var result = _utils.IsAdminUser(string.Empty);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsAdminUser_ThrowsForNullInput()
+        {
+            // Method calls userName.ToLower() and will throw on null; assert that behavior.
+            if (!_utils.IsAdminUser(null))
+            {
+                Assert.Fail("Expected exception was not thrown.");
+            }    
+
+        }
+    }
+}
